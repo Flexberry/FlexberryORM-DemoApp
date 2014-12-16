780affa14fd24b3157beb8de2a7cf74db789c514
kuzkok <kuzkok@gmail.com>
2014-12-16 21:29:58 +0500

    parent classes

---
+++ nHibernate/nHibernateSample/Domain/D.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nHibernateSample.Domain
+{
+    public class D
+    {
+        public virtual System.Guid Primarykey { get; set; }
+        public virtual string Name { get; set; }
+        public virtual string S1 { get; set; }
+        public virtual string S2 { get; set; }
+        public virtual string S3 { get; set; }
+        public virtual string S4 { get; set; }
+        public virtual string S5 { get; set; }
+    }
+}
